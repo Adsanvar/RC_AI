b3c7302447b140b4fff055e44ff5466f6b497b45
Daniel Vega <dvega@prattmiller.com>
2021-06-26 22:30:19 -0400

    Added basic functionality to pub vehicle marker for visualization

--- src/vehicle_controller/include/vehicle_controller/vehicle_controller.hpp
+++ src/vehicle_controller/include/vehicle_controller/vehicle_controller.hpp
@@ -8,6 +8,9 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Float32.h>
+#include <visualization_msgs/Marker.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Vector3.h>
 #include "xbox_driver/XboxInterface.h"
 
 //Namespace commonly matches ROS package name
@@ -22,20 +25,25 @@ namespace vehicle_controller{
       //Node-specific stuff here.
 
       //Variables
-      xbox_driver::XboxInterface xbox_msg;
+      //xbox_driver::XboxInterface xbox_msg;
+      visualization_msgs::Marker vehicle_marker_;
+      geometry_msgs::TwistStamped vehicle_twist_;
+      geometry_msgs::Vector3 vehicle_dynamics_;
       std::string node_name_;
       int temp;
 
       //Subscribers
       ros::Subscriber sub_xbox_joy_cmds_;
+      ros::Subscriber sub_vehicle_dynamics_;
 
       //Publishers
-      //ros::Publisher pub_;
-      
+      ros::Publisher pub_vehicle_marker_;
+      ros::Publisher pub_mobility_cmd_;
+
       //Functions
-      void msgCallbackXboxCmds(const sensor_msgs::Joy::ConstPtr& msg);
-
-
+      void msgCallbackXboxCmds(const xbox_driver::XboxInterface::ConstPtr& msg);
+      //void msgCallbackPubMarker(const visualization_msgs::Marker::ConstPtr& msg);
+      void msgCallbackVehicleDynamics(const geometry_msgs::Vector3::ConstPtr& msg);
 
   };
 
--- src/vehicle_controller/src/vehicle_controller.cpp
+++ src/vehicle_controller/src/vehicle_controller.cpp
@@ -7,14 +7,17 @@ namespace vehicle_controller
   // Constructor with global and private node handle arguments
   Controls::Controls(ros::NodeHandle& n, ros::NodeHandle& pn)
   {
-  
+
     node_name_ = ros::this_node::getName();
 
     //Define Subscribers
-    sub_xbox_joy_cmds_ = n.subscribe<xbox_driver::XboxInterface>("/xbox_joy_cmds", 10, &Xbox::msgCallbackJoystick, this);
+    sub_xbox_joy_cmds_ = n.subscribe<xbox_driver::XboxInterface>("/xbox_joy_cmds", 10, &Controls::msgCallbackXboxCmds, this);
+    sub_vehicle_dynamics_ = n.subscribe<geometry_msgs::Vector3>("/vehicle_dynamics", 10, &Controls::msgCallbackVehicleDynamics, this);
 
     //Define Publishers
-    
+    pub_mobility_cmd_ = n.advertise<geometry_msgs::TwistStamped>("/MobilityCmd", 10);
+    pub_vehicle_marker_ = n.advertise<geometry_msgs::TwistStamped>("/VehicleMarker", 10);
+
     ROS_INFO("This node is running successfully"); //Print to console
   }
 
@@ -32,20 +35,82 @@ namespace vehicle_controller
     msg.Brake
     msg.Throttle
     */
-    float speed = 0.0;
-    //Convert Joystick commands to yaw and velocity
-    if(msg.Ignition == true)
-    {
-      speed = 0;
-    }else
-    {
-      speed = 0;
+
+    //Need to create and update state machine based on controller inputs
+
+    //Get ROS time
+    ros::Time timestamp = ros::Time::now();
+    vehicle_twist_.header.stamp = timestamp;
+
+    vehicle_twist_.twist.linear.x = msg->Throttle;
+    vehicle_twist_.twist.angular.z = msg->Steering;
+
+    pub_mobility_cmd_.publish(vehicle_twist_);
+
+    /*
+    //Initialize Header
+    //Check if vehicle is ON
+    if(msg.Ignition == true){
+
+      //Check if vehicle is in Park/Drive
+      if(msg.Drive == true){
+        vehicle_twist_.linear.x = msg.Throttle;
+        vehicle_twist_.angular.z = msg.Steering;
+      }
+
+      if(msg.Park == true){
+        vehicle_twist_.linear.x = 0;
+        vehicle_twist_.angular.z = 0;
+      }
+
+
+    }else{
+      //Do Nothing
     }
-    
-    //Publish Message
-   
+    */
+
+}
+
+  //Publisher Callback for Visualization of Marker in Rviz
+  void Controls::msgCallbackVehicleDynamics(const geometry_msgs::Vector3::ConstPtr& msg)
+  {
+
+    //Get ROS time
+    vehicle_marker_.header.stamp = ros::Time::now();
+
+    //Select Marker Parameters
+    vehicle_marker_.header.frame_id = "/my_frame";
+
+    vehicle_marker_.ns = "basic_shapes";
+    vehicle_marker_.id = 0;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    vehicle_marker_.type = visualization_msgs::Marker::CUBE;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    vehicle_marker_.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    vehicle_marker_.pose.position.x = msg->x;
+    vehicle_marker_.pose.position.y = msg->y;
+    vehicle_marker_.pose.position.z = 1;
+    vehicle_marker_.pose.orientation.x = 0.0;
+    vehicle_marker_.pose.orientation.y = 0.0;
+    vehicle_marker_.pose.orientation.z = 0.0;
+    vehicle_marker_.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    vehicle_marker_.scale.x = 2.0;
+    vehicle_marker_.scale.y = 1.0;
+    vehicle_marker_.scale.z = 1.0;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    vehicle_marker_.color.r = 0.0f;
+    vehicle_marker_.color.g = 1.0f;
+    vehicle_marker_.color.b = 0.0f;
+    vehicle_marker_.color.a = 1.0;
+
+    pub_vehicle_marker_.publish(vehicle_marker_);
+
   }
-
-  //void calculate_velocity()
-
 }
